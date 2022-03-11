b07f70b474d741a667924a41d7acdf262a296630
malickasso <101105450+malickasso@users.noreply.github.com>
2022-03-11 09:23:54 +0100

    fork

--- main.c
+++ main.c
@@ -1,65 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct node
-{
-    int val;//valeur
-    struct node *next;//pointeur
-};
-//struct node *init_node(void);
-struct node *addHead(struct node *head,int a)
-{
-    struct node *nouv=(struct node *)malloc(sizeof(struct node));
-    nouv->val=a;
-    nouv->next=head;
-    head = nouv;
-    return head;
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(int argc , char *argv[]){
+  pid_t pid = fork();
+  if(pid < 0){
 
-}
+    fprintf(stderr, "Erreur de création du processus (%d) (%d)\n", pid, errno);
+    return 1;
+  }
+  if(pid == 0){
 
+     printf("je suis le fils (%d), mon père est (%d)\n", getpid(), getppid());
+  }else{
 
-struct node *addTail(struct node *head,int a)
-{
-    if(head==NULL)
-    {
-        head=addHead(head,a);
-    }
-    else
-    {
-       struct node *nouv=(struct node *)malloc(sizeof(struct node));
-        nouv->val=a;
-        nouv->next=NULL;
-        struct node *ptr=head;
-        while(ptr->next!=NULL)
-        {
-            ptr=ptr->next;
-        }
-        ptr->next=nouv;
-    }
-        return head;
-}
-
-void display_list(struct node *head)
-{
-    if(head!=NULL)
-    {
-       struct node *ptr=head;
-        while(ptr!=NULL)
-        {
-            printf("%d->",ptr->val);
-            ptr=ptr->next;
-        }
-        printf("NULL");
-
-    }
-}
-
-
-int main()
-{
-   struct node *head=NULL;
-    head=addHead(head,3);
-    head=addHead(head,1);
-     head=addTail(head,5);
-    display_list(head);
-    getch();
+    int status;
+    pid_t pid2 = wait(&status);
+    printf("Je suis le père (%d) mon fils que je viens de créer (%d)"
+           "s'est terminé avec le code d'erreur (%d)", getpid(), pid, status);
+  }
+  printf("Ou suis-je?\n");
+  return 0;
 }
