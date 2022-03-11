41477774493fea294781645910eb0702c8145119
malickasso <101105450+malickasso@users.noreply.github.com>
2022-03-11 08:11:53 +0100

    fork.c

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main(int argc , char *argv[]){
+  pid_t pid = fork();
+  if(pid < 0){
 
-int main()
-{
-    int i,n,res;
-    printf("Donnez un nombre positif SVP : ");
-    scanf("%d", &n);
+    fprintf(stderr, "Erreur de création du processus (%d) (%d)\n", pid, errno);
+    return 1;
+  }
+  if(pid == 0){
 
-    res=1;
+     printf("je suis le fils (%d), mon père est (%d)\n", getpid(), getppid());
+  }else{
 
-    for(i=2;i<=n;i++)
-    {
-        res=res*i;
-    }
-
-    printf(" %d factorielle vaut %d", n, res);
-
-    return 0;
+    int status;
+    pid_t pid2 = wait(&status);
+    printf("Je suis le père (%d) mon fils que je viens de créer (%d)"
+           "s'est terminé avec le code d'erreur (%d)", getpid(), pid, status);
+  }
+  printf("Ou suis-je?\n");
+  return 0;
 }
